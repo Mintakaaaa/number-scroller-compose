bf8bf6cee1f6bdf90bacb28f382d7ac3a0b5ac66
Mindaugas Brazlauskas <mbrazlauskas1@sheffield.ac.uk>
2024-08-21 20:03:42 +0100

    just spent over an hour suffering

--- number-scroller-compose/build.gradle.kts
+++ number-scroller-compose/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
 
                 groupId = "com.github.mintakaaaa"
                 artifactId = "number-scroller-compose"
-                version = "1.7"
+                version = "1.8"
             }
         }
     }
