bdeecb92f0d0c8a557ff4791cb3f8748b707fd60
Mindaugas Brazlauskas <mbrazlauskas1@sheffield.ac.uk>
2024-08-23 12:16:20 +0100

    Version change

--- number-scroller-compose/build.gradle.kts
+++ number-scroller-compose/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.mintakaaaa"
             artifactId = "number-scroller-compose"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
